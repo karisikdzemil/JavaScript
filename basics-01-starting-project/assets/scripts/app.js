55f51942dc526fbcc6c2925ba790ab7943020165
dzemil karisik <karisikdzemil@gmail.com>
2024-07-21 15:17:08 +0200

    functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,9 +2,13 @@
 
 const defaultResult = 0;
 let currentResult = defaultResult;
- 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 - 1";
+
+function add (num1, num2){
+    const result = num1 + num2;
+    return result;
+}
+  
+currentResult = add(1, 2);
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
-
