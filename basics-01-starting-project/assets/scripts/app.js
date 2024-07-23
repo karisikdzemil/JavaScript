ed9460a4c34c2bb1d3c7443ae0732e2f514b3636
dzemil karisik <karisikdzemil@gmail.com>
2024-07-23 21:26:36 +0200

    connecting all buttons to finctions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,17 +3,42 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserNumberinput (){
-    return parseInt(userInput.value);
+function getUserNumberinput() {
+  return parseInt(userInput.value);
 }
 
-function add (){
-    const enteredNumber = getUserNumberinput()
-    let calcDesript = currentResult;    
-    currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, `${calcDesript} + ${enteredNumber}`);
+function createAndWritteOutput(operator, resultBefCalc, calcNumber) {
+  const calcDescrition = `${resultBefCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescrition);
 }
-  
+
+function add() {
+  const enteredNumber = getUserNumberinput();
+  let initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWritteOutput("+", initialResult, enteredNumber);
+}
+function substract() {
+  const enteredNumber = getUserNumberinput();
+  let initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWritteOutput("-", initialResult, enteredNumber);
+}
+function multiply() {
+  const enteredNumber = getUserNumberinput();
+  let initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWritteOutput("*", initialResult, enteredNumber);
+}
+function divide() {
+  const enteredNumber = getUserNumberinput();
+  let initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWritteOutput("/", initialResult, enteredNumber);
+}
+
 addBtn.addEventListener("click", add);
-console.log('test');
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 
