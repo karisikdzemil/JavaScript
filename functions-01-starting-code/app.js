ab663839390a731e69f7ba6f5d4bc80634d338b4
dzemil karisik <karisikdzemil@gmail.com>
2024-08-04 15:30:58 +0200

    start game

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,2 +1,26 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+const getPlayerChoice = function () {
+  const selection = prompt("Rock, Paper or Scissors", "").toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+    if(gameIsRunning){
+        return;
+    }
+    gameIsRunning = true;
+  console.log("Game is starting...");
+  const PlayerSelection = getPlayerChoice();
+  console.log(PlayerSelection);
+});
