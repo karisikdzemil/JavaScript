7af3e0b2d1db780b95ede0a465419cbe5f90bcfc
dzemil karisik <karisikdzemil@gmail.com>
2024-08-06 18:37:38 +0200

    dom assingment

---
+++ assignments/dom-assignment-start/assignment.js
@@ -0,0 +1,14 @@
+const firstTaskSelecting = document.getElementById("task-1");
+const firstTaskSelecting2 = document.querySelector("li");
+
+firstTaskSelecting.style.color = "white";
+firstTaskSelecting2.style.backgroundColor = "black";
+
+const secondTaskSelecting = document.querySelector("title");
+const secondTaskSelecting2 = document.querySelector("head");
+const selecting2 = secondTaskSelecting2.querySelector("title");
+
+const thirdTaskSelecting = document.querySelector("h1");
+thirdTaskSelecting.textContent = "Assingment - Solved!";
+
+selecting2.textContent = "Assingment - Solved!";
