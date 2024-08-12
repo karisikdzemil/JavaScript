430c4620c74fad8abbedefd3041e7c38ad99dd79
dzemil karisik <karisikdzemil@gmail.com>
2024-08-12 23:56:39 +0200

    find object in array and find index of object in an array

--- array-part/app.js
+++ array-part/app.js
@@ -44,3 +44,27 @@
 // HTMLFormControlsCollection.log(hobbies);
 
 
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// const storedResults = testResults.splice(2);
+const storedResults = testResults.concat([3.99, 2])
+
+testResults.push(5.91);
+
+console.log(testResults, storedResults);    
+console.log(testResults.indexOf(1.5));
+
+const personData = [{name: "Max"}, {name: "Manuel"}];
+console.log(personData.indexOf({name: "Manuel"}));
+
+const manuel = personData.find((person, idx, persons)=>{
+    return person.name === "Manuel";
+}) 
+
+manuel.name = "Anna";
+
+console.log(manuel, personData);
+
+const maxIndex = personData.findIndex((person, idx, persons)=>{
+    return person.name === "Max";
+})
+console.log(maxIndex);
