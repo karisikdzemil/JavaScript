9cffbde02db1c1e84a6a448b677961f233df68a2
dzemil karisik <karisikdzemil@gmail.com>
2024-08-12 23:18:38 +0200

    more of arrays

---
+++ array-part/app.js
@@ -0,0 +1,46 @@
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// const moreNumbers = new Array(2, 5);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers)
+
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const arrayItems = Array.from(listItems);
+// console.log(arrayItems);
+
+// const hobbies = ["coocing", "sports"];
+// const personalData = [30, "Max", {moreDetail: []}];
+
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+// for(data of analyticsData){
+//     for(dataPoint of data){
+//             console.log(dataPoint);
+//     }
+// }
+// console.log(personalData[1]);
+ 
+
+// const hobbies = ["sports", 'coocing'];
+// hobbies.push("reading");
+// hobbies.unshift("coding");
+// const popedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
+
+// hobbies[1] = "Coding";
+// // hobbies[5] = "Reading";  rareli used
+// console.log(hobbies, hobbies[4]);
+
+// hobbies.splice(1, 0, "Good Food");
+// console.log(hobbies);
+
+// const removedElements = hobbies.splice(-2, 1);
+// HTMLFormControlsCollection.log(hobbies);
+
+
