d787c5a308a74e8a261904771d7e0859b0166e40
dzemil karisik <karisikdzemil@gmail.com>
2024-08-14 12:59:35 +0200

    reduce, split, join

--- array-part/app.js
+++ array-part/app.js
@@ -87,6 +87,19 @@
 // })
 // console.log(taxAdjustPrices);
 
+// console.log(prices, taxAdjustPrices);
+
+//     function transformToObjects(numberArray) {
+//         const newArr = [];
+//     const logic = numberArray.map((value, index, arr)=>{
+//         newArr.push({val: value});
+//     })
+//     }
+// transformToObjects([1, 2, 3])
+
+
+
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
@@ -94,4 +107,43 @@ const taxAdjustPrices = prices.map((price, idx, prices)=>{
         const priceObj = {index: idx, taxAdjustPrices:price * (1 + tax)};
         return priceObj;
 })
-console.log(prices, taxAdjustPrices);
+
+const sortedPrice = prices.sort((a, b)=>{
+    if(a > b){
+        return 1;
+    }else if (a === b){
+        return 0;
+    }else{
+        return -1;
+    }
+})
+// console.log(sortedPrice.reverse());
+console.log(sortedPrice);
+
+const filteredArray = prices.filter((price, index, prices)=>{
+    return price > 6;
+});
+console.log(filteredArray)
+
+// let sum = 0;
+
+// prices.forEach((price)=>{
+//     sum += price;
+// })
+// console.log(sum)
+
+const sum = prices.reduce((prevValue, curValue, curIndex, prices)=>{
+    return prevValue + curValue;
+}, 0)
+
+console.log(sum);
+
+const data = "new york;10.99;2000";
+
+const transformedData = data.split(";");
+transformedData[1] = +transformedData[1];
+console.log(transformedData);
+
+const nameFragments = ["Max", "Schwartz"];
+const name = nameFragments.join(" ");
+console.log(name);
