552d653a6e5a26f1e6e3ffc28ab61e19ccff7ce3
dzemil karisik <karisikdzemil@gmail.com>
2024-08-16 21:22:09 +0200

    rendering movies functionality

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -3,6 +3,30 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = () =>{
+    const movieList = document.getElementById("movie-list");
+
+    if(movies.length === 0){
+        movieList.classList.remove("visible");
+        return;
+    }else{
+        movieList.classList.add("visible");
+    }
+    movieList.innerHTML = ``;
+
+    movies.forEach((movie)=>{
+        const movieEl = document.createElement("li");
+        let text = movie.info.title + " - ";
+        for(const key in movie.info){
+            if(key !== "title"){
+                text = text + `${key}: ${movie.info[key]}`;
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    })
+}
+
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
@@ -24,7 +48,7 @@ const addMovieHandler = () => {
     id: Math.random()
   }
   movies.push(newMovie);
-  console.log(newMovie);
+  renderMovies();
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler);
