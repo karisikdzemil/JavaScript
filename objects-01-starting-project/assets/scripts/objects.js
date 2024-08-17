70dfe86f3867b0ef98e056e4155c92efb1adaf0e
dzemil karisik <karisikdzemil@gmail.com>
2024-08-17 22:41:43 +0200

    filter function

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -3,29 +3,33 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () =>{
-    const movieList = document.getElementById("movie-list");
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById("movie-list");
 
-    if(movies.length === 0){
-        movieList.classList.remove("visible");
-        return;
-    }else{
-        movieList.classList.add("visible");
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    let text = movie.info.title + " - ";
+    for (const key in movie.info) {
+      if (key !== "title") {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
     }
-    movieList.innerHTML = ``;
-
-    movies.forEach((movie)=>{
-        const movieEl = document.createElement("li");
-        let text = movie.info.title + " - ";
-        for(const key in movie.info){
-            if(key !== "title"){
-                text = text + `${key}: ${movie.info[key]}`;
-            }
-        }
-        movieEl.textContent = text;
-        movieList.append(movieEl);
-    })
-}
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+};
 
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
@@ -42,13 +46,19 @@ const addMovieHandler = () => {
 
   const newMovie = {
     info: {
-        title, 
-        [extraName]: extraValue
+      title,
+      [extraName]: extraValue,
     },
-    id: Math.random()
-  }
+    id: Math.random(),
+  };
   movies.push(newMovie);
   renderMovies();
 };
 
-// addMovieBtn.addEventListener("click", addMovieHandler);
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm); 
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
