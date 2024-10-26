0d8157980c273d52982e27606495fb1bd9b565d9
dzemil karisik <karisikdzemil@gmail.com>
2024-10-26 20:04:42 +0200

    starting new part of course, oop in javascript

---
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        title: "A Pillow",
+        imageUrl: "https://linensociety.com/cdn/shop/products/heirloom-pillow_14294b09-8232-4d5d-a305-022b802669ab_1400x.jpg?v=1584402804",
+        price: 19.99,
+        description: "A soft pillow!"
+    },
+    {
+        title: "A Carpet",
+        imageUrl: "https://images.jdmagicbox.com/quickquotes/images_main/-92mbg0w7.jpg",
+        price: 89.99,
+        description: "A carpet which you might like - or not."
+    },
+]
