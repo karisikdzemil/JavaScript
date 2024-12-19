9ee27e6aeca819a0d20788dc2434fae55d35e98e
dzemil karisik <karisikdzemil@gmail.com>
2024-12-19 17:56:39 +0100

    deep dive in numbers

--- adv-fn-01-starting-setup/app.js
+++ adv-fn-01-starting-setup/app.js
@@ -58,3 +58,66 @@ userName = "Manuel";
 
 greetUser();
 
+// function powerOf(x, n){
+//     let result = 1;
+
+//     for(let i = 0; i < n; i++){
+//         result *= x;
+//     }
+//     return result;
+// }
+
+// console.log(powerOf(2, 3));
+
+function powerOf(x, n){
+//    if(n === 1){
+//     return x;
+//    }
+//    return x + powerOf(x, n-1);
+return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 3));
+
+const myself = {
+    name: "Max",
+    friends: [
+        {
+            name: "Manuel",
+            friends: [
+                {
+                    name: "Cris",
+                   friends: [
+                    {
+                        name: "Amilia",
+                        friends: [
+                            {
+                                name: "Julia"
+                            }
+                        ]
+                    }
+                   ]
+                }
+            ]
+        },
+        {
+        name: "Julia"
+        }
+    ]
+};
+
+function getFriendNames(person){
+    const collectedNames = [];
+
+    if(!person.friends){
+        return [];
+    }
+
+    for(friend of person.friends){
+        collectedNames.push(friend.name);
+        collectedNames.push(...getFriendNames(friend));
+    }
+    return collectedNames;
+}
+
+console.log(getFriendNames(myself)); 
